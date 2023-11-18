5ab4ea059331ecc9ab43c091ea453bd2fc64df24
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-11-18 13:34:38 +0100

    fix bugs

--- f_audio.py
+++ f_audio.py
@@ -1,12 +1,24 @@
 from openai import OpenAI
 import os
+import base64
 
 def get_audio(format):
-    os.system("base64 -D -i audio.txt -o audio."+format)
-
+    # format = "m4a"
+    # os.system("base64 -D -i audio.txt -o audio."+format)
     client = OpenAI(
         api_key='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'
     )
+
+    input_file = open("audio.txt", "r")
+    audio_file_encoded = input_file.read()
+    audio_file_decoded = base64.b64decode(audio_file_encoded)
+    input_file.close()
+
+    # print(audio_file_decoded)
+
+    output_file = open("audio."+format, "wb")
+    output_file.write(audio_file_decoded)
+    output_file.close()
 
     audio_file = open("audio."+format, "rb")
 
@@ -17,6 +29,8 @@ def get_audio(format):
         # language="it"
     )
 
+    audio_file.close()
+
     return transcript
 
-# audio_product()
+# get_audio()
