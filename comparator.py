8f35b678d4f17be1cb33494dbff90f9e3fdd6c1c
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-11-19 11:12:42 +0100

    new penny supermarket

--- comparator.py
+++ comparator.py
@@ -3,7 +3,7 @@ import requests
 
 def getTotal(list):
     global tot_carr
-    global tot_pam
+    global tot_conad
 
     pam_list = ''
     carr_list = ''
@@ -13,16 +13,16 @@ def getTotal(list):
         pam_list = pam_list + " -- " + product_pam
         carr_list = carr_list + " -- " + product_carr
 
-    result_pam = "{:.2f}".format(tot_pam)
+    result_pam = "{:.2f}".format(tot_conad)
     result_carr = "{:.2f}".format(tot_carr)
 
     # print(result_pam)
     # print(result_carr)
 
     Dictionary = {}
-    Dictionary['Pam'] = result_pam
+    Dictionary['Penny'] = result_pam
     Dictionary['Carrefour'] = result_carr
-    Dictionary['Pam_list'] = pam_list
+    Dictionary['Penny_list'] = pam_list
     Dictionary['Carr_list'] = carr_list
     
     print(Dictionary)
@@ -30,7 +30,7 @@ def getTotal(list):
 
 def comparaProdotto(product):
     global tot_carr
-    global tot_pam
+    global tot_conad
 
     url = f"https://www.carrefour.it/search?q={product}&srule=price-low-to-high"
     result = requests.get(url).text
@@ -50,31 +50,34 @@ def comparaProdotto(product):
     # print("PRICE_NUM:", price_num2)
 
     tot_carr = tot_carr + price_num2
-    product_carr = "Product: " + str(name2) + " " + "Price: " + str(price_num2)
+    product_carr = "Product: " + str(name2) + ", " + "Price: " + str(price_num2)
 
-    print(product_carr)
+    # print(product_carr)
 
-    url = f"https://pamacasa.pampanorama.it/spesa-consegna-domicilio/20135/ricerca?search={product}&sort=asc"
+    url = f"https://pennyacasa.it/ricerca?search={product}&sort=asc"
     result = requests.get(url).text
     doc = BeautifulSoup(result, "html.parser")
     
-    # print("########## PAM")
+    # print("########## PENNY")
 
     name = doc.find_all(itemprop="name")
-    name1 = name[1]
-    name_str1 = str(name1).split('"')[-4]
-    # print(name_str1)
+    name1 = name[3]
+    name_str1 = str(name1).split('=')[-2]
+    name_str2 = str(name_str1).split('"')[-2]
+    # print(name_str2)
 
-    brand1 = doc.find(class_="brand-name").text
-    # print(brand1.strip())
+    brand1 = "Conad"
+    # print(brand1)
 
     price1 = doc.find(class_="price").text.strip()
     # print(price1)
-    price_num1 = float(str(price1).split("€")[-2].replace(',','.'))
-    # print("PRICE_NUM:", price_num1)
+    price_num1 = float(str(price1).split(" ")[-2].replace(',','.'))
+    # print(price_num1)
+    
 
-    tot_pam = tot_pam + price_num1
-    product_pam = "Product: " + name_str1 + " " + "Price: " + str(price_num1)
+    tot_conad = tot_conad + price_num1
+
+    product_pam = "Product: " + name_str2 + ", " + "Price: " + str(price_num1)
     #fine ricerca pam
 
     # print("\n\nBUY HERE: ")
@@ -87,7 +90,7 @@ def comparaProdotto(product):
     return (product_pam, product_carr)
 
 #main
-tot_pam = 0
+tot_conad = 0
 tot_carr = 0
 
 # products = "salsa,pomodoro,miele"
