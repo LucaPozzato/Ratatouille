a04c1f9d1b3a10dd537bf3cc4e1c97e0663e2e78
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-11-19 04:38:06 +0100

    added json api

--- app.py
+++ app.py
@@ -104,6 +104,35 @@ def del_product():
 
 @app.post("/pantry/identify")
 def identify_product():
+    # if request.is_json:
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    # If the user submits an empty part without selecting a file, ignore it
+    if file.filename == '':
+        return 'No selected file'
+
+    # Process the uploaded file
+    file_content = file.read()
+
+    # Convert the file content to base64
+    base64_content = base64.b64encode(file_content).decode('utf-8')
+
+    global id_product
+
+    # print(base64_content)
+
+    id_product = identifier.get_product(base64_content)
+    product_dict = json.loads(id_product)
+
+    print(product_dict)
+    return jsonify(product_dict), 200
+    # return {"error": "Request must be JSON"}, 415
+
+@app.post("/pantry/identify")
+def identify_product_json():
     if request.is_json:
         to_id = request.get_json()
         image = to_id["image"]
