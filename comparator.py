17d50752459fde81c346bc4eb97f18a986ebe84a
LucaPozzato <65533078+LucaPozzato@users.noreply.github.com>
2023-11-19 09:16:46 +0100

    changed pam/carr

--- comparator.py
+++ comparator.py
@@ -32,6 +32,28 @@ def comparaProdotto(product):
     global tot_carr
     global tot_pam
 
+    url = f"https://www.carrefour.it/search?q={product}&srule=price-low-to-high"
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, "html.parser")
+    
+    # print("\n\n########## CARREFOUR")
+
+    name2 = doc.find(class_="tile-description").text.strip()
+    # print(name2)
+
+    brand2 = doc.find(class_="brand").text.strip()
+    # print(brand2)
+
+    price2 = doc.find(class_="value").text.strip()
+    # print(price2)
+    price_num2 = float(str(price2).split("€")[-1].split(" ")[-1].replace(',','.'))
+    # print("PRICE_NUM:", price_num2)
+
+    tot_carr = tot_carr + price_num2
+    product_carr = "Product: " + str(name2) + " " + "Price: " + str(price_num2)
+
+    print(product_carr)
+
     url = f"https://pamacasa.pampanorama.it/spesa-consegna-domicilio/20135/ricerca?search={product}&sort=asc"
     result = requests.get(url).text
     doc = BeautifulSoup(result, "html.parser")
@@ -54,27 +76,6 @@ def comparaProdotto(product):
     tot_pam = tot_pam + price_num1
     product_pam = "Product: " + name_str1 + " " + "Price: " + str(price_num1)
     #fine ricerca pam
-
-
-    url = f"https://www.carrefour.it/search?q={product}&srule=price-low-to-high"
-    result = requests.get(url).text
-    doc = BeautifulSoup(result, "html.parser")
-    
-    # print("\n\n########## CARREFOUR")
-
-    name2 = doc.find(class_="tile-description").text.strip()
-    # print(name2)
-
-    brand2 = doc.find(class_="brand").text.strip()
-    # print(brand2)
-
-    price2 = doc.find(class_="value").text.strip()
-    # print(price2)
-    price_num2 = float(str(price2).split("€")[-1].split(" ")[-1].replace(',','.'))
-    # print("PRICE_NUM:", price_num2)
-
-    tot_carr = tot_carr + price_num2
-    product_carr = "Product: " + str(name2) + " " + "Price: " + str(price_num2)
 
     # print("\n\nBUY HERE: ")
 
